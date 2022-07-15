ccfe07e114f214f5d31eda039acdcf19eec05603
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-07-15 09:19:09 +0200

    The UI setup for sanity and the database to be integrated into the app

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,8 +1,23 @@
 import { TailwindProvider } from "tailwindcss-react-native";
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import {
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
+import Categories from "./components/Categories";
+import FeaturedRow from "./components/FeaturedRow";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -14,18 +29,63 @@ const HomeScreen = () => {
   // });
 
   return (
-    <SafeAreaView>
-      <View className=" bg-gray-800 h-full w-full">
-        {/* <Text className="text-red-500">HomeScreen</Text> */}
-        {/* Image */}
+    <SafeAreaView className="bg-white pt-6">
+      <View className="flex-row items-center mt-4 px-4 space-x-2">
         <Image
           source={{
             uri: "https://links.papareact.com/wru",
           }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        {/*  */}
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
       </View>
+
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 ">
+        <View className="flex-1 flex-row items-center p-2 bg-gray-200 rounded-lg space-x-2">
+          <SearchIcon />
+          <TextInput
+            className="flex-1"
+            placeholder="Restaurant and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsIcon color="#00CCBB" />
+      </View>
+
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {/* Categories component  */}
+        <Categories />
+        {/* Featured component  */}
+        <FeaturedRow
+          title="Featured"
+          description="Paid placements from our partners"
+          id="123"
+        />
+        <FeaturedRow
+          title="Tasty Discounts"
+          description="Paid placements from our partners"
+          id="1234"
+        />
+        <FeaturedRow
+          title="Offers near your"
+          description="Paid placements from our partners"
+          id="12345"
+        />
+      </ScrollView>
+
       <StatusBar style={"dark"} />
     </SafeAreaView>
   );
